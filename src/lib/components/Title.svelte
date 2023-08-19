b85a185340c83203f4cd7001da514d7a77758b9a
Jakku Night <J97k107u45k117n@gmail.com>
2023-08-19 13:27:19 -0400

    Finished title effect.

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 	export let href:string|undefined = "";
-	export let delay = 0;
+	export let delay = 10;
 	let element:HTMLElement;
 	let container:HTMLElement;
 	let text = "";
@@ -12,19 +12,32 @@
 		begin: "『 ",
 		end: " 』"
 	}
+	const charMap = (index = 0, length = 1):number => (Math.trunc(length / 100 * index) % length);
 	const show = () => {
-		if(i >= 100){
+		if(i >= 99){
 			clearInterval(pid);
 		}
 		container.style.left = `${50 - i / 2}%`;
 		element.style.width = `${i}%`;
 		if(
-			text[Math.round(text.length / 100 * i)] !== element.innerText[element.innerText.length - 1] &&
-			text[Math.round(text.length / 100 * i)] !== undefined
+			i > 0 &&
+			charMap(Math.trunc(50 - (i / 2)), text.length) !== charMap(Math.trunc(50 - (i + 1) / 2), text.length) &&
+			charMap(Math.trunc(50 + (i / 2)), text.length) !== charMap(Math.trunc(50 - (i - 1) / 2), text.length)
 		){
-			currentText += text[Math.round(text.length / 100 * i)];
-			element.innerText = currentText;
+			console.log("text:", text);
+			console.log("current:", currentText);
+			console.log("index:", i);
+			console.log("map:", Math.trunc(50 - (i / 2)), text.length);
+			
+			currentText = text[charMap(Math.trunc(50 - i / 2), text.length)] + currentText + text[charMap(Math.trunc(50 + i / 2), text.length)];
 		}
+		if(i === 0){
+			currentText = text[charMap(50, text.length)];
+		}
+		if(i === 99){
+			currentText = text;
+		}
+		element.innerText = currentText;
 		i++;
 	};
 	const display = () => {
@@ -36,12 +49,12 @@
 		display();
 	});
 </script>
-<span class="flex flex-row flex-nowrap w-fit h-auto justify-center items-center">
+<span class="flex w-fit h-auto justify-center items-center">
 	<span id={href} bind:this={container} class="relative font-bold text-neon-cyan-300" >
 		<span>
 			{decorators.begin}
 		</span>
-		<span bind:this={element} class="text-clip overflow-hidden w-0">
+		<span bind:this={element} class="text-clip overflow-clips w-0">
 			<slot/>
 		</span>
 		<span>
